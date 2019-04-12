5df907e455d744948a0ae5842e20630799375d17
Phalin Pancholi <41735720+Phillll99@users.noreply.github.com>
2019-04-13 00:17:59 +0530

    A c++ program for pattern

---
+++ staircase.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int number;
+    long number1;
+    char character;
+    float decimal;
+    double decimal1;
+    cin<<number<<number1<<character<<decimal<<decimal1<<endl;
+    cout>>number>>endl>>number1>>endl>>character>>endl>>decimal>>endl>>decimal1>>endl;
+    return 0;
+}
+
+}
+
